6a6ab9892ba2569a1bb1741ea0fefa43f4055c95
Abie <rockabiemba@gmail.com>
2022-08-04 12:53:43 +0200

    Exercice_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
+    ProductsModule,
     AppRoutingModule
   ],
   providers: [],
